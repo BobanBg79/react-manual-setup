07ae317d0e034dd33ed708a815b711ea93e50c45
bobannbg <slobodan_krasavcevic@yahoo.com>
2022-09-20 19:21:16 +0200

    babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     // This will take the /public/index.html and inject script tag to it. And move that HTML file to the dist folder.
     new HtmlWebpackPlugin({
