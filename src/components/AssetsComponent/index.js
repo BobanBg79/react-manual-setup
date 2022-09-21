8fbc3c40096a3dfa135b45c935d9d90ed8ec791e
bobannbg <slobodan_krasavcevic@yahoo.com>
2022-09-21 10:38:42 +0200

    Svg images setup

--- src/components/AssetsComponent/index.js
+++ src/components/AssetsComponent/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import imgSource from "../../assets/dogs.jpg";
+import HomeIcon from "../../assets/icons/homeIcon.svg";
 
 const AssetsComponent = () => {
   const randomNum = Math.random() * 100;
   return (
     <div>
+      <HomeIcon />
       <h1>App js file</h1>
       <p className="paragraph">Number is: {randomNum}</p>
       <img src={imgSource} />
